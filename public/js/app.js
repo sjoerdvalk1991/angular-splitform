850246551795132bfdffdee3a52954aae93d1d18
sjoerdvalk1991 <sjoerdvalk@gmail.com>
2014-12-16 13:55:34 +0100

    Update app.js
    I've made another version based on this article
    
    http://www.technofattie.com/2014/03/21/five-guidelines-for-avoiding-scope-soup-in-angular.html

--- public/js/app.js
+++ public/js/app.js
@@ -16,3 +16,26 @@
         }
     }]);
 })();
+
+//I've made another version to avoid an scope soup
+
+var app = angular.module('myApp', []);
+    var FormController = function($scope){
+        var _this = this;
+
+        this.step = 1;
+
+        this.nextStep = function(){
+            _this.step++;
+        }
+
+        this.prevStep = function(){
+            _this.step--;
+        }
+
+        this.submitForm = function(){
+
+        }
+    }
+FormController.$inject = ['$scope'];    
+app.controller('FormController', FormController);
